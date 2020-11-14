d4a52617bcb68e6f3e181ce970d708b5773cdf44
Alexandre Blanchet <blanchet_a0904@outlook.fr>
2020-11-14 15:52:57 +0100

    fonctions S1 Alex

--- carte.h
+++
@@ -1,2 +0,0 @@
-//Prototypes des fonctions et procédures du fichier carte.c
-int nombre_carte(_TJoueur leJoueur); // fonction qui retourne le nombre de cartes d’un joueur
--- init.h
+++
@@ -1,3 +0,0 @@
-//Prototypes des fonctions et procédures du fichier init.c
-void piocher_carte(_TJoueur * leJoueur, _TPioche pioche); //Procédure qui va prendre un numéro dans la pioche, créer la carte et la mettre dans le deck des joueurs
-void init_partie(_TJoueur * leJoueur, _TPioche pioche); // Procédure qui initialise les dés, les cartes ainsi que le pseudo d’un joueur
